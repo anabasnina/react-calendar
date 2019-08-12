fb28f09524ec1c121c09369c9f4518680abec63f
anabasnina <tbanina@bk.ru>
2019-08-12 09:34:58 +0300

    Add project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Calendar from "./components";
+
+function App() {
+  return <Calendar />;
+}
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
